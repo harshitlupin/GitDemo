bf6287a01f75e782daf375046bb002a46dd34f8a
Archit Kumar <architkumar314@gmail.com>
2024-05-08 18:34:17 +0530

    akk

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let string = "";
 for (let i = 0; i < n; i++) {
   for (let j = 0; j < i; j++) {
     string += "Archit";
+    string += "harshit";
+    console.log(string);
   }
   string += "\n";
 }
